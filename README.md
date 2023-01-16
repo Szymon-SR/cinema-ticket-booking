e25c5e19e2e31063149ffdd309d2d4b1e78967b5
Szymon-SR <srszymonsr@gmail.com>
2023-01-16 14:51:10 +0100

    Contact form template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Run in development:
-```flask --app src/booking run```
+```flask --app src/booking --debug run```
+
+--debug flag activates auto reloading on code changes.
--- src/booking.py
+++ src/booking.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('main.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact_form.html')
