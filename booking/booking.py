94507e01c8a6041504658baeb73af20f701602d4
weronika <weronika.lit0@gmail.com>
2023-01-19 23:37:06 +0100

    templates done

--- booking/booking.py
+++ booking/booking.py
@@ -16,3 +16,29 @@ def hello_world():
 @bp.route("/contact")
 def contact():
     return render_template('contact_form.html')
+
+@bp.route("/login")
+def login():
+    return render_template('user_login.html')
+
+@bp.route("/employee")
+def employee():
+    return render_template('employee_account.html')
+
+@bp.route("/employee/contact")
+def employee_contact_form():
+    shows_mocks = ["xd", "xd"]
+    return render_template('contact_form_records.html', all_messages = shows_mocks)
+
+@bp.route("/employee/reservations")
+def employee_reservations():
+    shows_mocks = ["xd", "xd"]
+    return render_template('reservations_records.html', all_reservations = shows_mocks)
+
+@bp.route("/employee/contact/answer")
+def employee_answer():
+    return render_template('contact_form_answer.html')
+
+@bp.route("/employee/reservations/details")
+def employee_reservation_details():
+    return render_template('reservation_details.html')
