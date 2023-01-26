62a3fb829e4c3a39a7fb8f223adc07e6bfe4c71d
wer-lit <weronika.lit0@gmail.com>
2023-01-26 15:36:40 +0100

    thread

--- booking/db.py
+++ booking/db.py
@@ -11,7 +11,8 @@ def get_db():
         g.db = sqlite3.connect(
             DATABASE_PATH,
             detect_types=sqlite3.PARSE_DECLTYPES,
-            check_same_thread=False
+
+        connect_args={"check_same_thread": False}
         )
         g.db.row_factory = sqlite3.Row
 
